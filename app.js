597aff76ed351b8935d8e9e5334afd296deb871b
taxpayer131 <chris.stefan1844@gmail.com>
2017-06-12 13:57:30 -0700

    regular commit to save work

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '12pm', '1pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var timesOfDay = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: ', 'Total: '];
 
-var pikePlaceMarket = {
-  locationName: 'Pike Place Market',
-  minCustomersPerHour: 23,
-  maxCustomerPerHour: 65,
-  avgCookiesPerCustomer: 6.3,
-  // calcCustomersThisHour: function() {},
-  // customersEachHour: [],
-  // calcCookiesThisHour: function() {},
-  // cookiesEachHour: [],
-  // render: function() {
-var liEL = document.createElement('li');
-}
-};
-
-liEl.textContent = pikePlaceMarket;
+var firstAndPike = {
+  name: '1st and Pike',
+  minCust: 23,
+  maxCust: 65,
+  avgCookieSale: 6.3,
+  custPerHour: function (){
+    var customers = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+    return customers;
+    console.log(customers);
+  },
