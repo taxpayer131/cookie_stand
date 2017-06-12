85f1768c45eb46eea43a5cbe77e3cd69cf180195
taxpayer131 <chris.stefan1844@gmail.com>
2017-06-12 14:25:35 -0700

    regular commit to save work

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-var timesOfDay = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: ', 'Total: '];
+var timesOfDay = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm'];
 
-var firstAndPike = {
-  name: '1st and Pike',
+var pikePlace = {
+  name: 'Pikes Market',
   minCust: 23,
   maxCust: 65,
-  avgCookieSale: 6.3,
-  custPerHour: function (){
-    var customers = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  avgCookiesPerSale: 6.3,
+  calcCustPerHour: function (){
+    var customers = Math.floor(Math.random() * (maxCust - minCust) + minCust);
     return customers;
     console.log(customers);
   },
+  cookiesPerHour: function(){
+    var hourlySales = [];
+    for(var i = 0; i < 15; i++){
+      var sales = this.custPerHour() * this.avgCookiesPerSale;
+      hourlySales.push(Math.ceil(sales));
+    }
