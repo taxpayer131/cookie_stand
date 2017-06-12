f51d4ebce9ae73ef8ee99c984c80540db204238d
taxpayer131 <chris.stefan1844@gmail.com>
2017-06-12 13:15:30 -0700

    modified app.js and added assets directory

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '12pm', '1pm', '3pm', '4pm', '5pm', '6pm'];
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '12pm', '1pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 var pikePlaceMarket = {
   locationName: 'Pike Place Market',
   minCustomersPerHour: 23,
   maxCustomerPerHour: 65,
   avgCookiesPerCustomer: 6.3,
-  calcCustomersThisHour: function() {},
-  customersEachHour: [],
-  calcCookiesThisHour: function() {},
-  cookiesEachHour: [],
-  render: function() {}
+  // calcCustomersThisHour: function() {},
+  // customersEachHour: [],
+  // calcCookiesThisHour: function() {},
+  // cookiesEachHour: [],
+  // render: function() {
+var liEL = document.createElement('li');
+}
 };
+
+liEl.textContent = pikePlaceMarket;
