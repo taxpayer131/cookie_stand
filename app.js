548226a80876cfce89799ff3fcd07a9771c6f876
taxpayer131 <chris.stefan1844@gmail.com>
2017-06-14 16:15:00 -0700

    finished table

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 'use strict';
+var myTable = document.getElementById('stores');
+var allStores = [];
 
 //Array for the different times of day
-var timesOfDay = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm:', 'Grand Total:'];
-//Variables ready to make the individual store objects
-var pikePlace = new Store('Pikes Market', 23, 65, 6.3);
-var seaTac = new Store ('Seatac', 3, 24, 1.2);
-var seaCent = new Store('Seattle Center', 11, 38, 3.7);
-var capHill = new Store('Capitol Hill', 30, 28, 2.3);
-var alki = new Store('Alki', 2, 16, 4.6);
+var timesOfDay = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm:'];
 
 //Constructor function
 function Store(name, minCust, maxCust, avgCookieSale) {
@@ -18,6 +14,9 @@ function Store(name, minCust, maxCust, avgCookieSale) {
   this.hourlySales = [];
   this.hourlyCust = [];
   this.totalSales = 0;
+  this.calcSalesByHour();
+  this.render();
+  allStores.push(this);
 };
 
 Store.prototype.calcCustPerHour = function ()  {
@@ -25,7 +24,6 @@ Store.prototype.calcCustPerHour = function ()  {
     var cust = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
     this.hourlyCust.push(cust);
   }
-  // return this.hourlyCust;
 };
 
 Store.prototype.calcSalesByHour = function(){
@@ -38,32 +36,45 @@ Store.prototype.calcSalesByHour = function(){
   return this.hourlySales;
 };
 
-//Rendering in JS for lab 7
-function Stores(name, minCust, maxCust, avgCookieSale, hourlySales, hourlyCust, totalSales){
-  var allStores = [];
+Store.prototype.render = function(){
+  var trEl = document.createElement('tr');
+  var tdEl = document.createElement('td');
+  tdEl.textContent = this.name;
+  trEl.appendChild(tdEl);
 
-  this.name = name;
-  this.minCust = minCust;
-  this.maxCust = maxCust;
-  this.avgCookieSale = avgCookieSale;
-  this.hourlySales = [];
-  this.hourlyCust = [];
-  this.totalSales = 0;
-  allStores.push(this);
+  for(var i = 0; i < timesOfDay.length; i++) {
+    tdEl = document.createElement('td');
+    tdEl.textContent = this.hourlySales[i];
+    trEl.appendChild(tdEl);
+  }
+
+  tdEl = document.createElement('td');
+  tdEl.textContent = this.totalSales;
+  trEl.appendChild(tdEl);
+  myTable.appendChild(trEl);
+};
+
+var thEl = document.createElement('th');
+var trEl = document.createElement('tr');
+thEl.textContent = 'Location';
+trEl.appendChild(thEl);
+for(var i = 0; i < timesOfDay.length; i++){
+  var thEl = document.createElement('th');
+  thEl.textContent = this.timesOfDay[i];
+  trEl.appendChild(thEl);
 }
 
-new Store('Pikes Market', 23, 65, 6.3);
-new Store ('Seatac', 3, 24, 1.2);
-new Store('Seattle Center', 11, 38, 3.7);
-new Store('Capitol Hill', 30, 28, 2.3);
-new Store('Alki', 2, 16, 4.6);
+thEl = document.createElement('th');
+thEl.textContent = 'Grand Total';
+trEl.appendChild(thEl);
+myTable.appendChild(trEl);
 
-function storesTable(){
-
+for(var i = 0; i < allStores.length; i++){
+  allStores[i].render();
 }
-//calls on each store function to run
-// createList(pikePlace);
-// createList(seaTac);
-// createList(seaCent);
-// createList(capHill);
-// createList(alki);
+//Variables ready to make the individual store objects
+var pikePlace = new Store('Pikes Market', 23, 65, 6.3);
+var seaTac = new Store ('Seatac', 3, 24, 1.2);
+var seaCent = new Store('Seattle Center', 11, 38, 3.7);
+var capHill = new Store('Capitol Hill', 30, 28, 2.3);
+var alki = new Store('Alki', 2, 16, 4.6);
